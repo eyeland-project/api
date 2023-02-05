f92d61b774b8ed1dc347db155b4a7fe2c543d72b
Leonardo Vergara <leonardo11vergara11@gmail.com>
2023-02-05 13:16:27 -0500

    db init update: fix 1

--- database/init.sql
+++ database/init.sql
@@ -17,7 +17,7 @@ CREATE TABLE task (
     deleted BOOLEAN NOT NULL DEFAULT FALSE,
     -- CONSTRAINTS
     CONSTRAINT pk_task PRIMARY KEY (id_task),
-    CONSTRAINT uk_task_order UNIQUE (task)
+    CONSTRAINT uk_task_task_order UNIQUE (task_order)
 );
 
 -- create table links (pre-task)
@@ -27,8 +27,8 @@ CREATE TABLE link (
     topic VARCHAR(100) NOT NULL,
     url VARCHAR(2048) NOT NULL,
     -- CONSTRAINTS
-    CONSTRAINT pk_link PRIMARY KEY (id_link)
-    CONSTRAINT fk_task FOREIGN KEY (id_task) REFERENCES task(id_task)
+    CONSTRAINT pk_link PRIMARY KEY (id_link),
+    CONSTRAINT fk_link_task FOREIGN KEY (id_task) REFERENCES task(id_task)
 );
 
 -- CREATING TABLE preguntas
@@ -46,9 +46,8 @@ CREATE TABLE question (
     deleted BOOLEAN NOT NULL DEFAULT FALSE,
     -- CONSTRAINTS
     CONSTRAINT pk_question PRIMARY KEY (id_question),
-    CONSTRAINT fk_task FOREIGN KEY (id_task) REFERENCES task(id_task),
-    -- CONSTRAINT uk_constr UNIQUE (id_task, question_order, exam) -- should question_order be unique?
-    CONSTRAINT uk_constr UNIQUE (id_task, exam)
+    CONSTRAINT fk_question_task FOREIGN KEY (id_task) REFERENCES task(id_task),
+    CONSTRAINT uk_question_constr UNIQUE (id_task, question_order, exam)
 );
 
 -- CREATING TABLE respuestas
@@ -61,7 +60,7 @@ CREATE TABLE option (
     deleted BOOLEAN NOT NULL DEFAULT FALSE,
     -- CONSTRAINTS
     CONSTRAINT pk_option PRIMARY KEY (id_option),
-    CONSTRAINT fk_question FOREIGN KEY (id_question) REFERENCES question(id_question)
+    CONSTRAINT fk_option_question FOREIGN KEY (id_question) REFERENCES question(id_question)
 );
 
 -- create table Instituciones
@@ -76,7 +75,7 @@ CREATE TABLE institution (
     email VARCHAR(320) NOT NULL,
     -- CONSTRAINTS
     CONSTRAINT pk_institution PRIMARY KEY (id_institution),
-    CONSTRAINT uk_nit UNIQUE (nit)
+    CONSTRAINT uk_institution_nit UNIQUE (nit)
 );
 
 -- create table Profesores
@@ -90,9 +89,9 @@ CREATE TABLE teacher (
     id_institution SMALLSERIAL NOT NULL,
     -- CONSTRAINTS
     CONSTRAINT pk_teacher PRIMARY KEY (id_teacher),
-    CONSTRAINT fk_institution FOREIGN KEY (id_institution) REFERENCES institution(id_institution)
-    CONSTRAINT uk_email UNIQUE (email)
-    CONSTRAINT uk_username UNIQUE (username)
+    CONSTRAINT fk_teacher_institution FOREIGN KEY (id_institution) REFERENCES institution(id_institution),
+    CONSTRAINT uk_teacher_email UNIQUE (email),
+    CONSTRAINT uk_teacher_username UNIQUE (username)
 );
 
 -- create table Cursos
@@ -105,53 +104,53 @@ CREATE TABLE course (
     status BOOLEAN NOT NULL DEFAULT FALSE,
     -- CONSTRAINTS
     CONSTRAINT pk_course PRIMARY KEY (id_course),
-    CONSTRAINT fk_teacher FOREIGN KEY (id_teacher) REFERENCES teacher(id_teacher),
-    CONSTRAINT fk_institution FOREIGN KEY (id_institution) REFERENCES institution(id_institution)
+    CONSTRAINT fk_course_teacher FOREIGN KEY (id_teacher) REFERENCES teacher(id_teacher),
+    CONSTRAINT fk_course_institution FOREIGN KEY (id_institution) REFERENCES institution(id_institution)
+);
+
+-- create table Grupos
+CREATE TABLE team (
+    id_team SERIAL NOT NULL,
+    id_course SERIAL NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    code CHAR(6),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    -- CONSTRAINTS
+    CONSTRAINT pk_team PRIMARY KEY (id_team),
+    CONSTRAINT fk_team_course FOREIGN KEY (id_course) REFERENCES course(id_course)
 );
 
 -- create table Estudiantes
 CREATE TABLE student (
     id_student SERIAL NOT NULL,
     id_course SERIAL NOT NULL,
+    current_team INTEGER,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(320) NOT NULL,
     username VARCHAR(50) NOT NULL,
-    current_team SERIAL,
     blindness VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL,
     -- CONSTRAINTS
     CONSTRAINT pk_student PRIMARY KEY (id_student),
-    CONSTRAINT fk_course FOREIGN KEY (id_course) REFERENCES course(id_course)
-    CONSTRAINT fk_team FOREIGN KEY (current_team) REFERENCES team(id_team)
-    CONSTRAINT uk_email UNIQUE (email)
-    CONSTRAINT uk_username UNIQUE (username)
-    CONSTRAINT check_blindness CHECK (blindness IN ('total', 'partial', 'none'))
-);
-
--- create table Grupos
-CREATE TABLE team (
-    id_team SERIAL NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    code CHAR(6) NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT TRUE,
-    id_course SERIAL NOT NULL,
-    -- CONSTRAINTS
-    CONSTRAINT pk_team PRIMARY KEY (id_team),
-    CONSTRAINT fk_course FOREIGN KEY (id_course) REFERENCES course(id_course)
+    CONSTRAINT fk_student_course FOREIGN KEY (id_course) REFERENCES course(id_course),
+    CONSTRAINT fk_student_team FOREIGN KEY (current_team) REFERENCES team(id_team),
+    CONSTRAINT uk_student_email UNIQUE (email),
+    CONSTRAINT uk_student_username UNIQUE (username),
+    CONSTRAINT check_student_blindness CHECK (blindness IN ('total', 'partial', 'none'))
 );
 
 -- create table Estudiantes_Grupos
 CREATE TABLE student_team (
-    -- id_student_team SERIAL NOT NULL, -- not needed
+    id_student_team SERIAL NOT NULL,
     power VARCHAR(20) NOT NULL,
     id_student SERIAL NOT NULL,
     id_team SERIAL NOT NULL,
     -- CONSTRAINTS
     CONSTRAINT pk_student_team PRIMARY KEY (id_student_team),
-    CONSTRAINT fk_student FOREIGN KEY (id_student) REFERENCES student(id_student),
-    CONSTRAINT fk_team FOREIGN KEY (id_team) REFERENCES team(id_team)
-    CONSTRAINT check_power CHECK (power IN ('super_hearing', 'memory_pro', 'super_radar'))
+    CONSTRAINT fk_student_team_student FOREIGN KEY (id_student) REFERENCES student(id_student),
+    CONSTRAINT fk_student_team_team FOREIGN KEY (id_team) REFERENCES team(id_team),
+    CONSTRAINT check_student_team_power CHECK (power IN ('super_hearing', 'memory_pro', 'super_radar'))
 );
 
 -- create table Administradores
@@ -163,9 +162,9 @@ CREATE TABLE admin (
     username VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL,
     -- CONSTRAINTS
-    CONSTRAINT pk_admin PRIMARY KEY (id_admin)
-    CONSTRAINT uk_email UNIQUE (email)
-    CONSTRAINT uk_username UNIQUE (username)
+    CONSTRAINT pk_admin PRIMARY KEY (id_admin),
+    CONSTRAINT uk_admin_email UNIQUE (email),
+    CONSTRAINT uk_admin_username UNIQUE (username)
 );
 
 -- create table 
@@ -181,34 +180,35 @@ CREATE TABLE task_attempt (
     end_time TIMESTAMP NOT NULL,
     -- CONSTRAINTS
     CONSTRAINT pk_task_attempt PRIMARY KEY (id_task_attempt),
-    CONSTRAINT fk_task FOREIGN KEY (id_task) REFERENCES task(id_task),
-    CONSTRAINT fk_team FOREIGN KEY (id_team) REFERENCES team(id_team),
-    CONSTRAINT fk_student FOREIGN KEY (id_student) REFERENCES student(id_student)
+    CONSTRAINT fk_task_attempt_task FOREIGN KEY (id_task) REFERENCES task(id_task),
+    CONSTRAINT fk_task_attempt_team FOREIGN KEY (id_team) REFERENCES team(id_team),
+    CONSTRAINT fk_task_attempt_student FOREIGN KEY (id_student) REFERENCES student(id_student)
 );
 
 -- create table
-CREATE TABLE answer_attempt (
-    id_answer_attempt SERIAL NOT NULL,
+CREATE TABLE answer (
+    id_answer SERIAL NOT NULL,
     id_question SERIAL NOT NULL,
     id_option SERIAL NOT NULL,
     id_task_attempt SERIAL NOT NULL,
     count INTEGER NOT NULL,
     time_stamp TIMESTAMP NOT NULL,
     -- CONSTRAINTS
-    CONSTRAINT pk_answer_attempt PRIMARY KEY (id_answer_attempt),
-    CONSTRAINT fk_question FOREIGN KEY (id_question) REFERENCES question(id_question),
-    CONSTRAINT fk_option FOREIGN KEY (id_option) REFERENCES option(id_option),
-    CONSTRAINT fk_task_attempt FOREIGN KEY (id_task_attempt) REFERENCES task_attempt(id_task_attempt)
+    CONSTRAINT pk_answer PRIMARY KEY (id_answer),
+    CONSTRAINT fk_answer_question FOREIGN KEY (id_question) REFERENCES question(id_question),
+    CONSTRAINT fk_answer_option FOREIGN KEY (id_option) REFERENCES option(id_option),
+    CONSTRAINT fk_answer_task_attempt FOREIGN KEY (id_task_attempt) REFERENCES task_attempt(id_task_attempt)
 );
 
-CREATE TABLE answer_attempt_audio (
-    id_answer_attempt_audio SERIAL NOT NULL,
-    id_answer_attempt SERIAL NOT NULL,
+-- create table
+CREATE TABLE answer_audio (
+    id_answer_audio SERIAL NOT NULL,
+    id_answer SERIAL NOT NULL,
     topic VARCHAR(100) NOT NULL,
     url VARCHAR(2048) NOT NULL,
     -- CONSTRAINTS
-    CONSTRAINT pk_answer_attempt_audio PRIMARY KEY (id_answer_attempt_audio),
-    CONSTRAINT fk_answer_attempt FOREIGN KEY (id_answer_attempt) REFERENCES answer_attempt(id_answer_attempt)
+    CONSTRAINT pk_answer_audio PRIMARY KEY (id_answer_audio),
+    CONSTRAINT fk_answer_audio_answer FOREIGN KEY (id_answer) REFERENCES answer(id_answer)
 );
 
 -- TODO: create table Animal
@@ -265,8 +265,12 @@ INSERT INTO teacher (id_teacher, id_institution, first_name, last_name, email, u
 -- INSERT INTO Cursos
 INSERT INTO course (id_course, id_institution, id_teacher, name, description) VALUES (1, 1, 1, 'Curso de prueba', 'Curso de prueba para la aplicación');
 
+-- INSERT INTO Equipos
+INSERT INTO team (id_team, id_course, name) VALUES (1, 1, 'Equipo de prueba');
+
 -- INSERT INTO Estudiantes
-INSERT INTO student (id_student, id_course, first_name, last_name, email, username, password, current_team, blindness) VALUES (1, 1, 'Estudiante', 'Prueba', 'student@test.com', 'student', 'student', 1, 'none');
+INSERT INTO student (id_student, id_course, first_name, last_name, email, username, current_team, blindness, password) VALUES (1, 1, 'Estudiante', 'Prueba', 'student@test.com', 'student', 1, 'none', 'pass123');
+INSERT INTO student (id_student, id_course, first_name, last_name, email, username, current_team, blindness, password) VALUES (2, 1, 'Estudiante', 'Prueba', 'student2@test.com', 'student2', NULL, 'none', 'pass123');
 
 -- INSERT INTO Administradores
 INSERT INTO admin (id_admin, first_name, last_name, email, username, password) VALUES (1, 'Administrador', 'Prueba', 'admin@test.com', 'admin', 'pass123');
