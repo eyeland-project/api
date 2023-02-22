2cf7e8ee576712f200546b90a056be06f7d4a677
LeoLizc <ldlizcano@uninorte.edu.co>
2023-02-21 21:18:03 -0500

    shuffle function

---
+++ TSVersion/src/types/custom/array-extensions.ts
@@ -0,0 +1,16 @@
+import { pseudoRandom } from '../../utils';
+// array-extensions.ts
+export {};
+
+declare global {
+  interface Array<T> {
+    shuffle : (seed: number) => T[];
+  }
+}
+
+Array.prototype.shuffle = function<T>(seed: number): T[] {
+    return this.sort(() => pseudoRandom(seed) - 0.5);
+};
+// Array with random items
+Array.from({length: 6}, (_, k) => k + 1);
+
