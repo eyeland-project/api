407a77074b4f4f5a7e1bf73d50e94c08c787edc1
Leonardo Vergara <leonardo11vergara11@gmail.com>
2023-03-05 20:16:30 -0500

    duringtask available check removed

--- TSVersion/src/controllers/students/duringtask.controller.ts
+++ TSVersion/src/controllers/students/duringtask.controller.ts
@@ -27,7 +27,7 @@ export async function root(req: Request<{ taskOrder: number }>, res: Response<Du
 export async function getQuestion(req: Request<{ taskOrder: number, questionOrder: number }>, res: Response<DuringtaskQuestionResp>, next: Function) {
     try {
         const { id: idStudent } = req.user!;
-        if (!await duringtaskAvailable(idStudent)) throw new ApiError('DuringTask is not available', 400);
+        // if (!await duringtaskAvailable(idStudent)) throw new ApiError('DuringTask is not available', 400);
         const { taskOrder, questionOrder } = req.params;
         
         const { id_question, content, type, img_alt, img_url, audio_url, video_url } = await getQuestionByOrder(taskOrder, 2, questionOrder);
