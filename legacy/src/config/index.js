f8435ae841b0238f5a76c4046a5e9d47bd0984da
LeoLizc <ldlizcano@uninorte.edu.co>
2023-03-09 22:50:02 -0500

    legacy project removed

--- legacy/src/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(environment = "development") {
-    return {
-        session: require("./session-config")(environment),
-        database_conf: require("./database-config"),
-    }
-};
