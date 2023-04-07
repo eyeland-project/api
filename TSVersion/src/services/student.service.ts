b76769063d239df72f904628f30dea5728f810e6
Leonardo Vergara <leonardo11vergara11@gmail.com>
2023-04-07 00:48:03 -0500

    fix raffle power notification

--- TSVersion/src/services/student.service.ts
+++ TSVersion/src/services/student.service.ts
@@ -123,7 +123,7 @@ export async function rafflePower(idStudent: number) {
   // ** if the number is -1, return false
   if (randomIdx === -1) return false;
   // ** else, assign the power to the student
-  assignPower(idStudent, powers[randomIdx]);
+  await assignPower(idStudent, powers[randomIdx]);
 
   notifyCourseOfTeamUpdate(id_course, id_team, idStudent);
   notifyStudentOfTeamUpdate(idStudent);
