5b07d96080d2bee800e26cc75491b5a6da14d66f
itsjp10 <69181374+itsjp10@users.noreply.github.com>
2025-05-22 02:11:21 -0500

    Update jest.config.ts

--- TSVersion/jest.config.ts
+++ TSVersion/jest.config.ts
@@ -9,6 +9,18 @@ const config: Config.InitialOptions = {
   },
   moduleNameMapper: {
     '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@app$': '<rootDir>/src/app',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@models$': '<rootDir>/src/models/index',
+    '^@routes$': '<rootDir>/src/routes/index',
+    '^@utils$': '<rootDir>/src/utils',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@dto/(.*)$': '<rootDir>/src/dto/$1',
+    '^@listeners/(.*)$': '<rootDir>/src/listeners/$1',
+    '^@database/(.*)$': '<rootDir>/src/database/$1'
   },
   testMatch: [
     '**/tests/**/*.test.ts',
