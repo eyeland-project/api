749fac29801fa802550401234162e3dfe117eb51
Jean Pierre <jalvarezidep@gmail.com>
2025-05-29 23:39:41 -0500

    Cambios en las pruebas

--- TSVersion/tests/routes.test/Students/users.test.ts
+++ TSVersion/tests/routes.test/Students/users.test.ts
@@ -1,22 +1,22 @@
-import { api } from "@utils";
+import { api } from "../utils";
 
 describe("(Students) Auth endpoints", () => {
   describe("POST /auth/login", () => {
     it("should return a status 200", async () => {
       await api
-        .post("students/auth/login")
+        .post("/api/students/login")
         .send({
           username: "student1",
-          password: "student1"
+          password: "pass123"
         })
         .expect(200)
         .expect("Content-Type", /application\/json/);
     });
 
     it("should return a token", async () => {
-      const response = await api.post("students/auth/login").send({
+      const response = await api.post("/api/students/login").send({
         username: "student1",
-        password: "student1"
+        password: "pass123"
       });
 
       expect(response.body.token).toBeDefined();
@@ -24,10 +24,10 @@ describe("(Students) Auth endpoints", () => {
 
     it("should return a 401 error", async () => {
       const response = await api
-        .post("students/auth/login")
+        .post("/api/students/login")
         .send({
           username: "student1",
-          password: "student2"
+          password: "wrongpassword"
         })
         .expect(401)
         .expect("Content-Type", /application\/json/);
