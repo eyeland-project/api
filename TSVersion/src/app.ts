662dd252aa1f862564292a8af56e3e9fba5b3dcd
Jean Pierre <jalvarezidep@gmail.com>
2025-06-04 00:04:12 -0500

    no se puede separar en produccion, revocar cambios de dotenv

--- TSVersion/src/app.ts
+++ TSVersion/src/app.ts
@@ -1,16 +1,16 @@
-//* Charge environmental variables SOLO EN DESARROLLO LOCAL
-if (process.env.NODE_ENV !== "production") {
-  const dotenv = require("dotenv");
-  const path = require("path");
-  dotenv.config({ override: true });
-  if (process.env.NODE_ENV) {
-    const result = dotenv.config({
-      path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)
-    });
-    if (result.error) {
-      console.log(process.env.NODE_ENV);
-      // throw result.error;
-    }
+/// <reference path="./interfaces/custom/customTypes.d.ts" />
+//* Charge environmental variables
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+dotenv.config({ override: true });
+if (process.env.NODE_ENV) {
+  const result = dotenv.config({
+    path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)
+  });
+  if (process.env.NODE_ENV !== "production" && result.error) {
+    console.log(process.env.NODE_ENV);
+    // throw result.error;
   }
 }
 
